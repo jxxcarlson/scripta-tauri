0cce6e6c2e89b7dd7c763ce1d189f311cb9616b9
James Carlson <jxxcarlson@gmail.com>
2022-09-21 17:11:02 -0400

    Write files to ~/Desktop/scripta

--- build.sh
+++ build.sh
@@ -1,3 +1,6 @@
 elm make src/Main.elm --output=out/main.js --optimize
 cp public/index.html out/
 cp public/katex-custom-element.js out/
+cp public/tauri.js out/
+cp public/katex.min.js out/
+cp public/katex.min.css out/
